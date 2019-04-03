60446b7032c959300a62500525c954a84c96d7d9
apgpavel <apgpavel@gmail.com>
2019-04-03 15:02:25 +0200

    Create RewardPoints.py

---
+++ RewardPoints.py
@@ -0,0 +1,29 @@
+# pass test 28 minutes
+
+from collections import defaultdict
+
+class RewardPoints:
+    def __init__(self):
+        self.customers = defaultdict(int)
+
+    def earn_points(self, customer_name, points):
+        if self.customers[customer_name] == 0:
+            self.customers[customer_name] += 500
+        if points <= 0:
+            points = 0
+
+        self.customers[customer_name] += points
+
+    def spend_points(self, customer_name, points):
+        if customer_name not in self.customers:
+            return "0"
+
+        if (points <= 0 or points > self.customers[customer_name]):
+            points = 0
+
+        self.customers[customer_name] -= points
+        return self.customers[customer_name]
+
+rewardPoints = RewardPoints()
+rewardPoints.earn_points('John', 520)
+print(rewardPoints.spend_points('John', 200))
